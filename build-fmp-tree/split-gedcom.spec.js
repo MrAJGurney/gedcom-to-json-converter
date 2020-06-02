062e3f6f7a1ed1d9c1dcedd0b45751c5055de827
Andrew Gurney <AGurney@FindMyPast.com>
2020-06-02 11:46:20 +0100

    Improve gedcom line splitting tests

--- build-fmp-tree/split-gedcom.spec.js
+++ build-fmp-tree/split-gedcom.spec.js
@@ -3,92 +3,42 @@
 const { splitGedcom, } = require('./split-gedcom');
 
 describe('splitGedcom', () => {
-	const gedcomFilesContents    = [
-		[
-			[
-				'',
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'',
-				'2 NAME Findmypast Family Tree',
-				'',
-				'2 VERS 2.0',
-				'',
-				'',
-			].join('\n'),
-			[
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'2 NAME Findmypast Family Tree',
-				'2 VERS 2.0',
-			],
-		],
-		[
-			[
-				'',
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'',
-				'2 NAME Findmypast Family Tree',
-				'',
-				'2 VERS 2.0',
-				'',
-				'',
-			].join('\r'),
-			[
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'2 NAME Findmypast Family Tree',
-				'2 VERS 2.0',
-			],
-		],
-		[
-			[
-				'',
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'',
-				'2 NAME Findmypast Family Tree',
-				'',
-				'2 VERS 2.0',
-				'',
-				'',
-			].join('\n\r'),
-			[
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'2 NAME Findmypast Family Tree',
-				'2 VERS 2.0',
-			],
-		],
-		[
-			[
-				'',
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'',
-				'2 NAME Findmypast Family Tree',
-				'',
-				'2 VERS 2.0',
-				'',
-				'',
-			].join('\r\n'),
-			[
-				'0 HEAD',
-				'1 SOUR FINDMYPAST',
-				'2 NAME Findmypast Family Tree',
-				'2 VERS 2.0',
-			],
-		],
+	const generateGedcomDocumentWithLineSeperator = lineSeperator => {
+		return [
+			'',
+			'0 HEAD',
+			'1 SOUR FINDMYPAST',
+			'',
+			'2 NAME Findmypast Family Tree',
+			'',
+			'2 VERS 2.0',
+			'',
+			'',
+		].join(lineSeperator);
+	};
+
+	const expectedLines = [
+		'0 HEAD',
+		'1 SOUR FINDMYPAST',
+		'2 NAME Findmypast Family Tree',
+		'2 VERS 2.0',
 	];
 
-	describe('when given gedcom file contents', () => {
-		it.each(gedcomFilesContents)(
-			'splits the gedcom by line',
-			(gedcomFileContents, expectedGedcomLines) => {
-				const actualGedcomLines = splitGedcom(gedcomFileContents);
+	describe('with a gedcom document', () => {
+		it.each([
+			{ lineSeperator: '\n', },
+			{ lineSeperator: '\r', },
+			{ lineSeperator: '\n\r', },
+			{ lineSeperator: '\r\n', },
+		])(
+			'builds the expected collection of non empty lines',
+			({ lineSeperator, }) => {
+				const gedcomDocument
+					= generateGedcomDocumentWithLineSeperator(lineSeperator);
 
-				expect(actualGedcomLines).toStrictEqual(expectedGedcomLines);
+				const actualLines = splitGedcom(gedcomDocument);
+
+				expect(actualLines).toStrictEqual(expectedLines);
 			}
 		);
 	});
