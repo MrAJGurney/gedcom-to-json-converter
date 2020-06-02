fe566a9ee27667778c64c7e3efe7aa338742d652
Andrew Gurney <AGurney@FindMyPast.com>
2020-06-02 11:22:23 +0100

    Improve date created builder tests

--- build-fmp-tree/build-fmp-date-created.spec.js
+++ build-fmp-tree/build-fmp-date-created.spec.js
@@ -3,65 +3,81 @@
 const { buildFmpDateCreated, } = require('./build-fmp-date-created');
 
 describe('buildFmpDateCreated', () => {
-	const structuredGedcoms = [
-		[
-			{
-				CHAN: [{
-					DATE: [{
-						value: {
-							level: 2,
-							lineValue: '10 May 2010',
-							tag: 'DATE',
-							xrefId: null,
-						},
-					}, ],
+	const dateOnlyBSTcase = {
+		gedcom: {
+			CHAN: [{
+				DATE: [{
 					value: {
-						level: 1,
-						lineValue: null,
-						tag: 'CHAN',
-						xrefId: null,
+						lineValue: '10 Jul 2020',
 					},
 				}, ],
-			},
-			'2010-05-10T00:00:00',
-		],
-		[
-			{
-				CHAN: [{
-					DATE: [{
-						TIME: [{
-							value: {
-								level: 3,
-								lineValue: '16:38:50',
-								tag: 'TIME',
-								xrefId: null,
-							},
-						}, ],
-						value: {
-							level: 2,
-							lineValue: '15 APR 2020',
-							tag: 'DATE',
-							xrefId: null,
-						},
-					}, ],
-					value: '1 CHAN',
-					value: {
-						level: 1,
-						lineValue: null,
-						tag: 'CHAN',
-						xrefId: null,
-					},
-				}, ],
-			},
-			'2020-04-15T16:38:50',
-		],
-	];
+			}, ],
+		},
+		expectedDateCreated: '2020-07-10T00:00:00',
+	};
 
-	describe('when given structured gedcom', () => {
-		it.each(structuredGedcoms)(
-			'builds a \'DateCreated\' property',
-			(structuredGedcom, expectedDateCreated) => {
-				const actualDateCreated = buildFmpDateCreated(structuredGedcom);
+	const dateOnlyGMTcase = {
+		gedcom: {
+			CHAN: [{
+				DATE: [{
+					value: {
+						lineValue: '25 Nov 2020',
+					},
+				}, ],
+			}, ],
+		},
+		expectedDateCreated: '2020-11-25T00:00:00',
+	};
+
+	const dateAndTimeBSTCase = {
+		gedcom: {
+			CHAN: [{
+				DATE: [{
+					TIME: [{
+						value: {
+							lineValue: '16:38:50',
+						},
+					}, ],
+					value: {
+						lineValue: '15 APR 2020',
+					},
+				}, ],
+			}, ],
+		},
+		expectedDateCreated: '2020-04-15T16:38:50',
+	};
+
+	const dateAndTimeGMTCase = {
+		gedcom: {
+			CHAN: [{
+				DATE: [{
+					TIME: [{
+						value: {
+							lineValue: '11:21:35',
+						},
+					}, ],
+					value: {
+						lineValue: '5 DEC 2020',
+					},
+				}, ],
+			}, ],
+		},
+		expectedDateCreated: '2020-12-05T11:21:35',
+	};
+
+	describe('with gedcom containing a change property', () => {
+		it.each([
+			// BST in 2020:
+			// - starts 29th of March
+			// - ends 25th of October
+			dateOnlyBSTcase,
+			dateOnlyGMTcase,
+			dateAndTimeBSTCase,
+			dateAndTimeGMTCase,
+		])(
+			'builds the expected date created property',
+			({ gedcom, expectedDateCreated, }) => {
+				const actualDateCreated = buildFmpDateCreated(gedcom);
 
 				expect(actualDateCreated).toStrictEqual(expectedDateCreated);
 			}
---
+++ build-fmp-tree/build-fmp-id-generator.spec.js
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('fmpIdGenerator', () => {
+	it.todo('todo');
+});
